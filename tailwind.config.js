5c87fa17688e11acd7a1a14a5cb4ffb23608b07d
StayAndGo <wthuck@163.com>
2022-09-25 22:33:51 +0800

    feat: init tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/*.html"],
+  theme: {
+    extend: {
+      padding:{
+        '8px': '8px',
+        '16px': '16px',
+      }
+    },
+  },
+  plugins: [],
+}
